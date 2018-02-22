08470762b653b3c0998d828161026f7f2da37581
Timo <harzzaa@gmail.com>
2018-02-22 02:20:30 +0200

    debugging

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Reactt</h1>
         </header>
         
         {this.state.locations.map(loc => {
